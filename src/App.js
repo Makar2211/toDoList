ef1ec144af612734b7f956eb8798f9896b10a5f8
Dovgopoly Makar <makardovgopolji@gmail.com>
2023-01-25 16:43:13 +0100

    toDoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import styles from './todoStyle.module.scss';
 function App() {
+  const [toDo, SetToDo] = React.useState([]);
+  const [input, setInput] = React.useState('');
+
+  const OnClickPush = () => {
+    if (input !== '') {
+      SetToDo([...toDo, input]);
+      setInput('');
+    }
+  };
+  const onDeleteTOdO = (t) => {
+    const deleteToDo = toDo.filter((dODelete) => {
+      return dODelete !== t;
+    });
+    SetToDo(deleteToDo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={styles.App}>
+        <h1>React TODO APP</h1>
+        <input
+          type='text'
+          placeholder='search todo'
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={OnClickPush}>отправить</button>
+        {toDo.length > 0 ? (
+          toDo.map((t, index) => {
+            return (
+              <div className={styles.todos}>
+                <li style={{ listStyleType: 'none' }} key={index}>
+                  {t}
+                </li>
+                <button onClick={() => onDeleteTOdO(t)}>Удалить</button>
+              </div>
+            );
+          })
+        ) : (
+          <h2>Вы ничего не добавили в toDo list</h2>
+        )}
+      </div>
+    </>
   );
 }
 
